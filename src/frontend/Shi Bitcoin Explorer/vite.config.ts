efcff708039fa978d4b72573ff5c39808d553aae
robdicoco <pavusa@gmail.com>
2025-01-27 17:49:02 -0300

    s1version

--- src/frontend/Shi Bitcoin Explorer/vite.config.ts
+++ src/frontend/Shi Bitcoin Explorer/vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
@@ -11,16 +13,23 @@ export default defineConfig({
     },
   },
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../keys/privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../keys/fullchain.pem')),
+    },
+    port: 5173, // Optional: Specify a port (default is 5173)
     proxy: {
       '/api': {
-        target: 'http://172.236.149.45:5000', // Backend server URL
+        target: 'https://shisatoshi.758206.xyz:5000', // Backend server URL
         changeOrigin: true, // Required for virtual hosted sites
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove `/api` prefix
+        secure: true, // Disable SSL verification (not recommended for production)
       },
       '/api2': {
         target: 'http://127.0.0.1:8000', // Backend server URL
         changeOrigin: true, // Required for virtual hosted sites
         rewrite: (path) => path.replace(/^\/api2/, ''), // Remove `/api2` prefix
+        secure: false, // Disable SSL verification (not recommended for production)
       },
     },
   },
